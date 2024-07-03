e2d47b7e369171efe88ca7a08f3bb400fa78a6f7
Mike haslam <ongomobile@gmail.com>
2024-07-02 17:20:09 -0700

    Initial Commit

---
+++ First Mac App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First Mac App
+//
+//  Created by Emorn on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First Mac App/First_Mac_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Mac_AppApp.swift
+//  First Mac App
+//
+//  Created by Emorn on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct First_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
